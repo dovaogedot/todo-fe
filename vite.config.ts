53be19087e4dd9bef79bb49bf5e6e31bca56ce44
dovaogedot <dovaogedot@gmail.com>
2024-03-26 01:30:49 +0200

    Removed 'server' from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      cert: fs.readFileSync(process.env.SSL_CRT || 'site.crt'),
-      key: fs.readFileSync(process.env.SSL_KEY || 'site.key'),
-    },
-    port: process.env.TODO_PORT,
-    host: process.env.TODO_HOST,
-  }
 })
